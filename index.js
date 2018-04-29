255c386ce5e63f8e7181e293f5d3cda23096afcd
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-04-29 16:01:05 +0200

    bugifx: fix invalid barrel import

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 exports.__esModule = true;
-__export(require("./lib"));
+__export(require("./dist"));
