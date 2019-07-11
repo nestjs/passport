8eadf5b8f8e0dbc126792a7b4583126f6e47aa2c
Chris Sidell <christophers@earny.co>
2019-07-11 14:37:57 -0700

    Add ability to pass in customParameters

--- lib/passport/passport.strategy.ts
+++ lib/passport/passport.strategy.ts
@@ -8,7 +8,18 @@ export function PassportStrategy<T extends Type<any> = any>(
   new (...args): T;
 } {
   abstract class MixinStrategy extends Strategy {
+    private readonly customParameters: {[key: string]: string};
     abstract validate(...args: any[]): any;
+
+    authenticate(...args: any[]): void {
+      const [req, params, ...rest] = args;
+      if(this.customParameters && typeof params === 'object' && !Array.isArray(params)) {
+        return void super.authenticate(req, Object.assign(params, this.customParameters), ...rest)
+      }
+
+      super.autenticate(...args);
+    }
+
     constructor(...args: any[]) {
       const callback = async (...params: any[]) => {
         const done = params[params.length - 1];
@@ -25,6 +36,12 @@ export function PassportStrategy<T extends Type<any> = any>(
       };
 
       super(...args, (...params: any[]) => callback(...params));
+
+      const [ config ] = args || [] as any;
+      if(config && config.customParameters) {
+        this.customParameters = config.customParameters;
+      }
+
       if (name) {
         passport.use(name, this as any);
       } else {
