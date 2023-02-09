f86fe8297d99202c4ba58528620a32117c28b037
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2023-02-09 09:31:38 +0100

    fix: revert auto-logging info

--- lib/auth.guard.ts
+++ lib/auth.guard.ts
@@ -91,9 +91,6 @@ function createAuthGuard(type?: string | string[]): Type<CanActivate> {
 
     handleRequest(err, user, info, context, status): TUser {
       if (err || !user) {
-        if (info) {
-          authLogger.error(info);
-        }
         throw err || new UnauthorizedException();
       }
       return user;
