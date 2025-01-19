84c8838c08633867c4a78ec40bef11828179e5cd
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2025-01-19 11:21:12 +0100

    fix: use all ctor params type to include overloads #1857

--- lib/passport/passport.strategy.ts
+++ lib/passport/passport.strategy.ts
@@ -1,6 +1,62 @@
 import * as passport from 'passport';
 import { Type, WithoutCallback } from '../interfaces';
 
+type ExcludeUnknown<T> =
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  T extends Array<infer I> ? ({} extends I & {} ? never : T) : T;
+export type AllConstructorParameters<T> = ExcludeUnknown<
+  T extends {
+    new (...o: infer U): void;
+    new (...o: infer U2): void;
+    new (...o: infer U3): void;
+    new (...o: infer U4): void;
+    new (...o: infer U5): void;
+    new (...o: infer U6): void;
+    new (...o: infer U7): void;
+  }
+    ? U | U2 | U3 | U4 | U5 | U6 | U7
+    : T extends {
+          new (...o: infer U): void;
+          new (...o: infer U2): void;
+          new (...o: infer U3): void;
+          new (...o: infer U4): void;
+          new (...o: infer U5): void;
+          new (...o: infer U6): void;
+        }
+      ? U | U2 | U3 | U4 | U5 | U6
+      : T extends {
+            new (...o: infer U): void;
+            new (...o: infer U2): void;
+            new (...o: infer U3): void;
+            new (...o: infer U4): void;
+            new (...o: infer U5): void;
+          }
+        ? U | U2 | U3 | U4 | U5
+        : T extends {
+              new (...o: infer U): void;
+              new (...o: infer U2): void;
+              new (...o: infer U3): void;
+              new (...o: infer U4): void;
+            }
+          ? U | U2 | U3 | U4
+          : T extends {
+                new (...o: infer U): void;
+                new (...o: infer U2): void;
+                new (...o: infer U3): void;
+              }
+            ? U | U2 | U3
+            : T extends {
+                  new (...o: infer U): void;
+                  new (...o: infer U2): void;
+                }
+              ? U | U2
+              : T extends {
+                    new (...o: infer U): void;
+                  }
+                ? U
+                : never
+>;
+
 abstract class PassportStrategyMixin<TValidationResult> {
   abstract validate(
     ...args: any[]
@@ -17,7 +73,7 @@ export function PassportStrategy<
   callbackArity?: true | number
 ): {
   new (
-    ...args: WithoutCallback<ConstructorParameters<T>>
+    ...args: WithoutCallback<AllConstructorParameters<T>>
   ): InstanceType<T> & PassportStrategyMixin<TValidationResult>;
 } {
   abstract class StrategyWithMixin
