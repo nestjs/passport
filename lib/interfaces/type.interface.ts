1a7123a285af7d932a05f642fcfae7a3dc1aeaff
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2025-01-23 13:44:51 +0100

    fix: exclude last argument only if its of type function (cb)

--- lib/interfaces/type.interface.ts
+++ lib/interfaces/type.interface.ts
@@ -4,4 +4,10 @@ export interface Type<T = any> extends Function {
 
 export type WithoutCallback<T extends any[]> =
   // T extends any makes this distributive
-  T extends any ? (T extends [...infer U, any] ? U : T) : never;
+  T extends any
+    ? T extends [...infer U, infer C]
+      ? C extends Function
+        ? U
+        : T
+      : T
+    : never;
